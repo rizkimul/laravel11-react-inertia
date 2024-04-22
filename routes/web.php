f8e7ae5d7abcf5c4ba3b95f42d056a6cc8b3e419
rizkimul <riizkimaul4@gmail.com>
2024-04-22 16:06:02 +0700

    add middleware group in routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::redirect('/', '/dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function() {
+    Route::get('/dashboard', function() {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::resource('project', ProjectController::class);
+    Route::resource('user', UserController::class);
+    Route::resource('task', TaskController::class);
 });
 
 Route::get('/dashboard', function () {
